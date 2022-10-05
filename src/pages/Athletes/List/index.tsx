7f49392a7e297ca7f820785a7c0fac00279b823c
John Vidal <ba.john.vidal@gmail.com>
2022-10-05 09:13:47 -0300

    v0.3.0

--- src/pages/Athletes/List/index.tsx
+++ src/pages/Athletes/List/index.tsx
@@ -1,74 +1,14 @@
 /* eslint-disable no-console */
-import {
-  ColumnDef,
-  useReactTable,
-  getCoreRowModel,
-  flexRender,
-} from '@tanstack/react-table';
 import { MdEdit, MdOutlineDeleteOutline } from 'react-icons/md';
 import { AiOutlineEye } from 'react-icons/ai';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { IconButton } from '../../../components/Inputs/IconButton';
 import { SelectBare } from '../../../components/Inputs/SelectBare';
 import { TextfieldBare } from '../../../components/Inputs/TextfieldBare';
-import { Button } from '../../../components/Inputs/Button';
 import { useGetAthletes } from '../../../dataAccess/hooks/athlete/useGetAllAthletes';
-import { IUser } from '../../../types/User';
 import { useRedirectPendingAthlete } from '../../../hooks/useRedirectPendingAthlete';
-
-const columns: ColumnDef<IUser>[] = [
-  {
-    accessorKey: 'name',
-    header: 'Nome',
-    cell: info => info.getValue(),
-  },
-  {
-    accessorKey: 'team',
-    header: 'Clube',
-    cell: info => {
-      const obj = info.getValue();
-
-      // @ts-ignore
-      return obj.name;
-    },
-  },
-  {
-    accessorKey: 'athleteProfile.sex',
-    header: 'Sexo',
-    cell: info => info.getValue() || 'Não cadastrado',
-  },
-  {
-    accessorKey: 'status',
-    header: 'Status',
-    cell: info => info.getValue(),
-  },
-  {
-    header: '',
-    accessorKey: 'id',
-    cell: info => (
-      <div className="flex gap-2 items-center justify-end">
-        <IconButton
-          icon={MdEdit}
-          className="text-light-primary"
-          size="1.5rem"
-          onClick={() => console.log(info.getValue())}
-        />
-        <IconButton
-          icon={AiOutlineEye}
-          className="text-light-tertiary"
-          size="1.5rem"
-          onClick={() => console.log(info.getValue())}
-        />
-        <IconButton
-          icon={MdOutlineDeleteOutline}
-          className="text-light-error"
-          size="1.5rem"
-          onClick={() => console.log(info.getValue())}
-        />
-      </div>
-    ),
-  },
-];
+import { useGetPublicFederations } from '../../../dataAccess/hooks/public/useGetPublicFederation';
+import { useGetPublicTeams } from '../../../dataAccess/hooks/public/useGetPublicTeams';
 
 const COLUMN_WIDTH = [
   'w-1/2 lg:w-1/4',
@@ -79,15 +19,39 @@ const COLUMN_WIDTH = [
   'w-auto',
 ];
 
+const COLUMN_NAMES = ['Nome', 'Clube', 'Sexo', 'Status', ''];
+
 export function AthletesListPage() {
   useRedirectPendingAthlete();
   const { data } = useGetAthletes();
+  const { data: publicFederation } = useGetPublicFederations();
+  const { data: publicTeams } = useGetPublicTeams();
 
-  const table = useReactTable({
-    data: data || [],
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
+  const [filter, setFilter] = useState('');
+  const [filterFederation, setFilterFederation] = useState('');
+  const [filterTeam, setFilterTeam] = useState('');
+
+  let tableData = data || [];
+
+  if (filterFederation) {
+    const teamsFederation = publicTeams?.list
+      .filter(team => team.federationId === filterFederation)
+      .map(team => team.id);
+
+    tableData = tableData.filter(athlete =>
+      teamsFederation?.includes(athlete.team!.id),
+    );
+  }
+
+  if (filterTeam) {
+    tableData = tableData.filter(athlete => athlete.team!.id === filterTeam);
+  }
+
+  if (filter) {
+    tableData = tableData.filter(athlete =>
+      athlete.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  }
 
   return (
     <div className="bg-light-surface p-6 rounded-2xl h-full">
@@ -98,66 +62,94 @@ export function AthletesListPage() {
       </div>
       <div className="flex flex-col lg:flex-row gap-2 mb-4">
         <div className="w-full lg:w-1/4">
-          <SelectBare label="Clube" name="team-filter">
-            <option value="">Todos</option>
-            <option value="">AABB - Canoas/RS</option>
-            <option value="">AABB/ São Leopoldo</option>
-            <option value="">Deodoro Hoquei Clube</option>
-          </SelectBare>
-        </div>
-        <div className="w-full lg:w-1/4">
-          {/* TODO: carregar dados do filtro do DB */}
-          <SelectBare label="Federação" name="federation-filter">
+          <SelectBare
+            label="Federação"
+            name="federation-filter"
+            onChange={e => setFilterFederation(e.target.value)}
+          >
             <option value="">Todas</option>
-            <option value="mg">
-              Federação de Hóquei do Estado de Minas Gerais
-            </option>
-            <option value="sc">
-              Federação de Hóquei do Estado de Santa Catarina
-            </option>
-            <option value="sp">
-              Federação de Hóquei do Estado de São Paulo
-            </option>
+            {publicFederation?.list.map(federation => (
+              <option key={federation.id} value={federation.id}>
+                {federation.name}
+              </option>
+            ))}
           </SelectBare>
         </div>
+
+        <div className="w-full lg:w-1/4">
+          <SelectBare
+            label="Clube"
+            name="team-filter"
+            onChange={e => setFilterTeam(e.target.value)}
+          >
+            <option value="">Todos</option>
+            {publicTeams?.list.map(team => (
+              <option value={team.id} key={team.id}>
+                {team.name}
+              </option>
+            ))}
+          </SelectBare>
+        </div>
+
         <div className="w-full lg:w-1/2">
-          <TextfieldBare label="Buscar..." name="search" />
+          <TextfieldBare
+            label="Buscar..."
+            name="search"
+            onChange={e => setFilter(e.target.value)}
+          />
         </div>
       </div>
       <table className="w-full">
         <thead>
-          {table.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header, index) => (
-                <th
-                  className={`${COLUMN_WIDTH[index]} text-left py-4 px-2 bg-slate-100`}
-                  key={header.id}
-                >
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )}
-                </th>
-              ))}
-            </tr>
-          ))}
+          <tr>
+            {COLUMN_NAMES.map((name, index) => (
+              <th
+                className={`${COLUMN_WIDTH[index]} text-left py-4 px-2 bg-slate-100`}
+                key={name}
+              >
+                {name}
+              </th>
+            ))}
+          </tr>
         </thead>
         <tbody>
-          {table.getRowModel().rows.map(row => (
+          {tableData?.map(athlete => (
             <tr
               className="border-b last:border-none border-slate-200"
-              key={row.id}
+              key={athlete.id}
             >
-              {row.getVisibleCells().map((cell, index) => (
-                <td
-                  className={`${COLUMN_WIDTH[index]} py-4 px-2`}
-                  key={cell.id}
-                >
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
+              <td className={`${COLUMN_WIDTH[0]} py-4 px-2`}>{athlete.name}</td>
+              <td className={`${COLUMN_WIDTH[1]} py-4 px-2`}>
+                {athlete.team?.name}
+              </td>
+              <td className={`${COLUMN_WIDTH[2]} py-4 px-2`}>
+                {athlete.athleteProfile?.gender}
+              </td>
+              <td className={`${COLUMN_WIDTH[3]} py-4 px-2`}>
+                {athlete.status}
+              </td>
+              <td className={`${COLUMN_WIDTH[4]} py-4 px-2`}>
+                <div className="flex gap-2 items-center justify-end">
+                  <IconButton
+                    icon={MdEdit}
+                    className="text-light-primary"
+                    size="1.5rem"
+                    onClick={() => console.log(athlete.id)}
+                  />
+                  <IconButton
+                    icon={AiOutlineEye}
+                    className="text-light-tertiary"
+                    size="1.5rem"
+                    onClick={() => console.log(athlete.id)}
+                  />
+                  <IconButton
+                    icon={MdOutlineDeleteOutline}
+                    className="text-light-error"
+                    size="1.5rem"
+                    onClick={() => console.log(athlete.id)}
+                  />
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>
