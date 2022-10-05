e8562b6a7ec1abd75ff0a4cc708938bed59a4ae3
John Vidal <ba.john.vidal@gmail.com>
2022-10-05 01:33:32 -0300

    fix: atleta pode navegar no sistema mesmo com a ficha incompleta

--- src/hooks/useRedirectPendingAthlete.ts
+++ src/hooks/useRedirectPendingAthlete.ts
@@ -1,16 +1,15 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { useGlobal } from '../contexts/global.context';
 import { Roles } from '../enums/Roles';
 import { Status } from '../enums/Status';
 
 export function useRedirectPendingAthlete() {
-  const navigate = useNavigate();
   const { user } = useGlobal();
 
   useEffect(() => {
     if (user?.role === Roles.USER && user?.status === Status.PENDING) {
-      navigate('/app/perfil');
+      toast.warning('Preencha sua ficha corretamente!');
     }
-  }, [navigate, user]);
+  }, [user]);
 }
