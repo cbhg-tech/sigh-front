e7ba2f8e317da6e5f63285534ea53d459697c310
John Vidal <ba.john.vidal@gmail.com>
2022-10-14 14:45:05 -0300

    feat: melhorando validacao de documento ja existente

--- src/services/validateIfDocumentExists.ts
+++ src/services/validateIfDocumentExists.ts
@@ -3,9 +3,14 @@ import { db } from '../app/FirebaseConfig';
 
 export async function validateIfDocumentExist(document: string) {
   const q = query(collection(db, 'users'), where('document', '==', document));
+  const qTc = query(
+    collection(db, 'technicalComittee'),
+    where('document', '==', document),
+  );
   const documentExist = await getDocs(q);
+  const documentExistTc = await getDocs(qTc);
 
-  if (documentExist.size > 0) {
+  if (documentExist.size > 0 || documentExistTc.size > 0) {
     throw new Error('Seu documento já está vinculado a algum time');
   }
 }
