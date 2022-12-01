588aaf4b1faab813bac63332b6a70beec60ff48a
John Vidal <ba.john.vidal@gmail.com>
2022-12-01 19:30:42 -0300

    fix: detalhe d eatle fica em branco

--- src/pages/Athletes/Details/index.tsx
+++ src/pages/Athletes/Details/index.tsx
@@ -46,7 +46,7 @@ export function AthleteDetailsPage() {
                 )}
               </div>
               <p className="text-2xl text-light-on-surface-variant">
-                {data?.related.name || 'Sem time'} -{' '}
+                {data?.related?.name || 'Sem time'} -{' '}
                 {defineAthleteCategory(data!.athleteProfile!.birthDate)}
               </p>
             </div>
