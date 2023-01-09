730b35a0690477e7d73448633f056e0e82f449d1
John Vidal <ba.john.vidal@gmail.com>
2023-01-09 14:19:42 -0300

    fix: arrumando scroll do sidenav

--- src/components/Navigation/Aside/index.tsx
+++ src/components/Navigation/Aside/index.tsx
@@ -42,7 +42,7 @@ export function Aside({ isOpen, toogleSideMenu }: IProps) {
     <aside
       className={`absolute duration-200 ease-in-out ${isOpenStyle} lg:translate-x-0 lg:p-4 top-0 left-0 z-10 w-full min-h-screen h-full lg:relative lg:w-full lg:col-span-1 bg-light-surface-1`}
     >
-      <div className="bg-light-surface-1 overflow-y-auto flex flex-col min-h-screen h-full p-4 lg:rounded-3xl">
+      <div className="bg-light-surface-1 overflow-y-auto flex flex-col min-h-screen h-full p-4 lg:rounded-3xl md:pb-20">
         <div className="flex justify-between mb-4 lg:hidden">
           <img
             className="w-20 object-contain"
